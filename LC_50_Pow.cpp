ab2d96a7552ca103ab31867adea397167a3120db
Pranav Mahajan <94169804+parumahajan@users.noreply.github.com>
2025-02-21 11:15:33 +0530

    Add files via upload

---
+++ LC_50_Pow.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double
+
+    return 0;
+}
+
+//______________________________
+// class Solution {
+// public:
+//     double myPow(double x, int n) {
+//         return pow(x,n);
+//     }
+// };
